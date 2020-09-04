314811b08f062ec5b640ac5bf648aef41a91e479
Rishi Raushan <rishi.raushan@sophos.com>
2020-09-05 02:42:40 +0530

    remove sku input data from the main source

--- src/main/java/com/promotion/engine/utils/PromotionEngineUtils.java
+++ src/main/java/com/promotion/engine/utils/PromotionEngineUtils.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.promotion.engine.model.AdderPromotion;
 import com.promotion.engine.model.MultiplierPromotion;
 import com.promotion.engine.model.SKUPrice;
-import com.promotion.engine.model.SKUUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
@@ -15,7 +14,6 @@ import java.io.IOException;
 import java.util.List;
 
 import static com.promotion.engine.utils.Constants.ADDER_SKU;
-import static com.promotion.engine.utils.Constants.INPUT_SKU_UNIT_FILE;
 import static com.promotion.engine.utils.Constants.MULTIPLIER_SKU;
 import static com.promotion.engine.utils.Constants.PROMOTIONS_FILE;
 import static com.promotion.engine.utils.Constants.SKU_UNIT_PRICE_FILE;
@@ -26,7 +24,6 @@ public class PromotionEngineUtils
 {
     public static final String PROMOTIONS;
     public static final String SKU_PRICE;
-    public static final String INPUT_SKU_UNIT;
     public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     static
@@ -35,9 +32,6 @@ public class PromotionEngineUtils
         {
             PROMOTIONS = IOUtils.toString(new ClassPathResource(PROMOTIONS_FILE).getInputStream(), UTF_8);
             SKU_PRICE = IOUtils.toString(new ClassPathResource(SKU_UNIT_PRICE_FILE).getInputStream(), UTF_8);
-            INPUT_SKU_UNIT = IOUtils.toString(new ClassPathResource(INPUT_SKU_UNIT_FILE).getInputStream(),
-                    UTF_8);
-
         }
         catch (IOException e)
         {
@@ -75,17 +69,6 @@ public class PromotionEngineUtils
         JsonNode skuPrices = actualObject.get("sku_price");
 
         return OBJECT_MAPPER.convertValue(skuPrices, new TypeReference<List<SKUPrice>>()
-        {
-        });
-    }
-
-    public static List<SKUUnit> getSkuUnits() throws IOException
-    {
-        JsonNode actualObject = OBJECT_MAPPER.readTree(INPUT_SKU_UNIT);
-
-        JsonNode skuPrices = actualObject.get("sku_unit");
-
-        return OBJECT_MAPPER.convertValue(skuPrices, new TypeReference<List<SKUUnit>>()
         {
         });
     }
